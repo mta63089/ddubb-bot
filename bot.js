4a6706ef7500418f93661b7a52a236e3200829ec
mta630 <mta630@ddubb.net>
2023-10-18 12:17:51 -0600

    added linting and formatting

--- bot.js
+++ bot.js
@@ -1,8 +1,12 @@
 // Require the discord library module
 const Discord = require('discord.js');
 
+// Define the intents to use
+const myIntents = new Discord.Intents();
+myIntents.add(Discord.Intents.FLAGS.Guilds, Discord.Intents.FLAGS.GuildMessages);
+
 // Creates a new Discord client
-const client = new Discord.Client();
+const client = new Discord.Client({intents: myIntents});
 
 // Set up an event listener that runs when the client is ready
 client.on('ready', () => {
@@ -10,7 +14,7 @@ client.on('ready', () => {
 });
 
 // Sets up another event listener that runs whenever a message is received.
-client.on('message', message => {
+client.on('messageCreate', message => {
     // log the message to the console
     console.log(message.content);
 });
